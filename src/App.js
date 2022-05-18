a8b38638dc2d93101c64ec385b1965bd668fc215
Divya Madala <71040191+divyamadala30@users.noreply.github.com>
2022-05-18 08:41:07 -0400

    hello world
    Signed-off-by: Divya Madala <71040191+divyamadala30@users.noreply.github.com>

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+/*
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { makeAutoObservable } from "mobx"
+import { observer } from "mobx-react"
+*/
+import './App.css';
+
+function App() {
+   return (
+      <div className="App">
+         Hello World !
+      </div>
+   );
+}
+export default App;
