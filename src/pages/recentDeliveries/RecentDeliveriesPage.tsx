10619a42b6a2e7296bd068a53defcbe788280b42
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2022-08-22 17:40:02 -0400

    Working but page still rendering too soon
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- src/pages/recentDeliveries/RecentDeliveriesPage.tsx
+++ src/pages/recentDeliveries/RecentDeliveriesPage.tsx
@@ -1,28 +1,20 @@
 import {
-  ApolloClient,
   useQuery,
-  InMemoryCache,
-  ApolloProvider,
-  NormalizedCacheObject,
-  Observable
 } from "@apollo/client";
 // import { Container, Nav, Form, Button, InputGroup } from "react-bootstrap";
 import "./RecentDeliveries.css";
 // import RecentDeliveriesTable from "./RecentDeliveriesTable";
 import {
   Request,
-  ExampleQueryDocument,
-  Query,
   RecentDeliveriesQueryDocument,
   RequestMetadataWhere
 } from "../../generated/graphql";
 import { observer } from "mobx-react";
 import { useEffect, useMemo } from "react";
-import { observable } from "mobx";
+import { makeAutoObservable, observable } from "mobx";
 import {InfiniteLoader, List} from "react-virtualized";
 import { Container, Form, InputGroup } from "react-bootstrap";
 import RecentDeliveriesTable from "./RecentDeliveriesTable";
-// import InfiniteScroll from "react-infinite-scroll-component";
 
 function resolveAndSortDeliveryDates(request: Request) {
   var smDataList: string[] = [];
@@ -52,38 +44,6 @@ export class RequestWithDate {
 }
 
 export const RecentDeliveriesPage: React.FunctionComponent = props => {
-  const filter = useMemo(() => {
-    const text = "";
-    return { text: text };
-  }, []);
-
-  const { client, loading, error, data } = useQuery(
-    RecentDeliveriesQueryDocument,
-    {
-      variables: {
-        where: {
-          igoRequestId_CONTAINS: filter.text
-        },
-        options: { limit: 20 }
-      }
-    }
-  );
-
-  function transformAndFilterRequestsByDate(requestsList: Array<Request>) {
-    var filteredRequests: RequestWithDate[] = [];
-    requestsList.forEach((r: Request) => {
-      filteredRequests.push(new RequestWithDate(r));
-    });
-    return filteredRequests;
-  }
-
-  if (loading) return <p>Loading requests...</p>;
-  if (error) return <p>Error : (</p>;
-
-  var filteredRequests: RequestWithDate[] = transformAndFilterRequestsByDate(
-    data.requests
-  );
-
   return (
     <Container>
       <RecentDeliveriesObserverable />
@@ -93,59 +53,83 @@ export const RecentDeliveriesPage: React.FunctionComponent = props => {
 
 export default RecentDeliveriesPage;
 
-const store = observable({ filter: "" });
+function createStore() {
+  return makeAutoObservable({
+    filter:""
+  })
+}
+
+const store = createStore();
 
 const RecentDeliveriesObserverable = observer(() => {
-  var filter = useMemo(() => {
-    return { text: "" };
-  }, []);
   const { client, loading, error, data, refetch, fetchMore } = useQuery(
     RecentDeliveriesQueryDocument,
     {
       variables: {
         where: {
-          igoRequestId_CONTAINS: filter.text
+          igoRequestId_CONTAINS: store.filter
+        },
+        requestsConnectionWhere2: {
+          igoRequestId_CONTAINS: store.filter
         },
         options: { limit: 20, offset: 0 }
       }
     }
   );
 
-  {
-    /* fetchMore({
-  variables: {
-    where: {
-      igoRequestId_CONTAINS: filter.text
-    },
-    options: { limit: 20, offset: 0 }
-  }
-}) */
-  }
-
-  function transformAndFilterRequestsByDate(requestsList: Array<Request>) {
-    var filteredRequests: RequestWithDate[] = [];
-    requestsList.forEach((r: Request) => {
-      filteredRequests.push(new RequestWithDate(r));
-    });
-    return filteredRequests;
-  }
+  // function transformAndFilterRequestsByDate(requestsList: Array<Request>) {
+  //   var filteredRequests: RequestWithDate[] = [];
+  //   requestsList.forEach((r: Request) => {
+  //     filteredRequests.push(new RequestWithDate(r));
+  //   });
+  //   return filteredRequests;
+  // }
 
   if (loading) return <p>Loading requests...</p>;
-  if (error) return <p>Error : (</p>;
+  if (error) return <p>Error :(</p>;
 
-  var filteredRequests: RequestWithDate[] = transformAndFilterRequestsByDate(
-    data.requests
-  );
-//   function getCacheRequests(){
-//     const ret = client.readQuery({
-//       query:RecentDeliveriesQueryDocument
-//     })
-//     console.log(ret);
-//     return ret || []
-// }
+  // var filteredRequests: RequestWithDate[] = transformAndFilterRequestsByDate(
+  //   data.requests
+  // );
+
+  function loadMoreRows({startIndex, stopIndex}, fetchMore: any) {
+    return fetchMore({
+        variables: {
+            // where: {
+            //     igoRequestId_CONTAINS: "00"
+            // },
+            options: {
+                offset: startIndex,
+                limit: stopIndex
+            }
+        }
+    });
+}
+
+  function isRowLoaded ({ index }) {
+    return index < data.requests.length;
+  }
+  function rowRenderer({key, index, style}) {
+    if (!data.requests[index]) {
+        if (index < remoteRowCount) {
+            return <div key={key} style={style}>{index} loading</div>;
+        } else {
+            return null;
+        }
+    }
+    return (
+        <div
+            key={key}
+            style={style}
+        >
+            {index} - {data.requests[index].igoRequestId}
+        </div>
+    )
+  }
+  const remoteRowCount = data.requestsConnection.totalCount;
   return (
     <Container>
-      <Container>Displaying {filter.text}</Container>
+      <Container>Displaying {store.filter}</Container>
       <InputGroup>
         <Form className="d-flex">
           <Form.Group>
@@ -153,20 +137,18 @@ const RecentDeliveriesObserverable = observer(() => {
               type="search"
               placeholder="Search"
               aria-label="Search"
-              // value={store.filter}
-              // value={filter.text}
               onInput={event => {
                 const value = String(
                   ((event.currentTarget as unknown) as HTMLInputElement).value
                 );
-                console.log(value);
                 if (value !== null) {
-                  console.log("filter value = ", value);
-                  filter.text = value;
-                  // store.filter = value;
+                  store.filter = value;
                   refetch({
                     where: {
-                      igoRequestId_CONTAINS: filter.text
+                      igoRequestId_CONTAINS: store.filter
+                    },
+                    requestsConnectionWhere2: {
+                      igoRequestId_CONTAINS: store.filter
                     },
                     options: { limit: 20, offset: 0 }
                   });
@@ -176,64 +158,28 @@ const RecentDeliveriesObserverable = observer(() => {
           </Form.Group>
         </Form>
       </InputGroup>
+
       <InfiniteLoader
-            isRowLoaded={(params)=>{
-                return isRowLoaded(params, data.requests);
-            }}
-            loadMoreRows={(params)=>{
-                return loadMoreRows(params, fetchMore)
-            }}
-            rowCount={filteredRequests.length}
-        >
-            {({ onRowsRendered, registerChild }) => (
-                <List
-                    height={200}
-                    onRowsRendered={onRowsRendered}
-                    ref={registerChild}
-                    rowCount={filteredRequests.length}
-                    rowHeight={20}
-                    rowRenderer={(props)=>{
-                      console.log(props)
-                        return rowRenderer(props,data.requests);
-                    }}
-                    width={300}
-                />
-            )}
-        </InfiniteLoader>
+                isRowLoaded={isRowLoaded}
+                loadMoreRows={(params) => {
+                    return loadMoreRows(params, fetchMore)
+                }}
+                rowCount={remoteRowCount}
+            >
+                {({onRowsRendered, registerChild}) => (
+                    <List
+                        height={200}
+                        onRowsRendered={onRowsRendered}
+                        ref={registerChild}
+                        rowCount={remoteRowCount}
+                        rowHeight={20}
+                        rowRenderer={rowRenderer}
+                        width={300}
+                    />
+                )}
+            </InfiniteLoader>
       {/* <RecentDeliveriesTable data={filteredRequests} /> */}
     </Container>
+    
   );
 });
-
-function isRowLoaded ({ index }, requests:any[]) {
-  return index < requests.length;
-}
-
-function loadMoreRows ({ startIndex, stopIndex }, fetchMore:any) {
-  console.log("loading more rows call - start index=",startIndex)
-  return fetchMore({
-      variables: {
-          // where: {
-          //     igoRequestId_CONTAINS: "00"
-          // },
-          options: {
-              offset:startIndex,
-              limit:20
-          }
-      }
-  });
-}
-
-function rowRenderer ({ key, index, style}, data) {
-
-  if (!data[index]) return <div key={key} style={style}>{index} loading</div>;
-  console.log("in row renderer func")
-  return (
-      <div
-          key={key}
-          style={style}
-      >
-          {index} - {data[index].igoRequestId}
-      </div>
-  )
-}
