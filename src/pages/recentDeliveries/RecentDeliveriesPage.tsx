91a32131ca1466577936ceb7ad6f5ab930633d5b
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2022-08-30 15:33:09 -0400

    Work in progress
    todo
    - get filtering to work (current workaround with click of button but not while hitting enter on keyboard while cursor in search bar)
    - add editing form that renders when "editingSampelId" is not undefined
    
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- src/pages/recentDeliveries/RecentDeliveriesPage.tsx
+++ src/pages/recentDeliveries/RecentDeliveriesPage.tsx
@@ -7,9 +7,11 @@ import {
 import { observer } from "mobx-react";
 import { makeAutoObservable } from "mobx";
 import { InfiniteLoader, Table, Column } from "react-virtualized";
-import { Container, Form, InputGroup, Row } from "react-bootstrap";
+import { Button, Container, Form, InputGroup, Row } from "react-bootstrap";
 import { TableCell } from "@material-ui/core";
 import { RequestSummaryObservable } from "../requestView/RequestSummary";
+import 'react-virtualized/styles.css';
+import {useState} from "react";
 
 function createStore() {
   return makeAutoObservable({
@@ -22,6 +24,8 @@ function createStore() {
 const store = createStore();
 
 export const RecentDeliveriesPage: React.FunctionComponent = props => {
+  
+
   function requestDetailsComponent() {
     console.log("selectedRequest=", store.selectedRequest);
     if (store.showRequestDetails) {
@@ -46,16 +50,18 @@ export const RecentDeliveriesPage: React.FunctionComponent = props => {
 
 export default RecentDeliveriesPage;
 
-const RecentDeliveriesObserverable = observer(() => {
+const RecentDeliveriesObserverable = () => {
+  const [val, setVal] = useState("");
+
   const { loading, error, data, refetch, fetchMore } = useQuery(
     RecentDeliveriesQueryDocument,
     {
       variables: {
         where: {
-          igoRequestId_CONTAINS: store.filter
+          requestJson_CONTAINS: store.filter
         },
         requestsConnectionWhere2: {
-          igoRequestId_CONTAINS: store.filter
+          requestJson_CONTAINS: store.filter
         },
         options: { limit: 20, offset: 0 }
       }
@@ -64,6 +70,7 @@ const RecentDeliveriesObserverable = observer(() => {
 
   if (loading) return <p>Loading requests...</p>;
   if (error) return <p>Error :(</p>;
+  
 
   function loadMoreRows({ startIndex, stopIndex }, fetchMore: any) {
     return fetchMore({
@@ -91,13 +98,14 @@ const RecentDeliveriesObserverable = observer(() => {
   }
 
   function headerRenderer({ dataKey }) {
+    // edit this to use actual col labels
     return <TableCell>{dataKey}</TableCell>;
   }
 
   function cellRenderer({ cellData }) {
     return (
       <TableCell align="right" padding="normal">
-        {cellData || ""}
+        {cellData || " "}
       </TableCell>
     );
   }
@@ -106,11 +114,10 @@ const RecentDeliveriesObserverable = observer(() => {
     console.log(info.rowData.igoRequestId);
     store.selectedRequest = info.rowData.igoRequestId;
     store.showRequestDetails = true;
-    console.log("onRowClick(), selectedRequest = ", store.selectedRequest);
   }
 
   const remoteRowCount = data.requestsConnection.totalCount;
-
+  
   return (
     <Row style={{}}>
       <InputGroup>
@@ -120,26 +127,32 @@ const RecentDeliveriesObserverable = observer(() => {
               type="search"
               placeholder="Search"
               aria-label="Search"
+              value={val}
               onInput={event => {
                 const value = String(
                   ((event.currentTarget as unknown) as HTMLInputElement).value
                 );
                 if (value !== null) {
-                  store.filter = value;
-                  refetch({
-                    where: {
-                      igoRequestId_CONTAINS: store.filter
-                    },
-                    requestsConnectionWhere2: {
-                      igoRequestId_CONTAINS: store.filter
-                    },
-                    options: { limit: 20, offset: 0 }
-                  });
+                  setVal(value);  
                 }
               }}
             />
           </Form.Group>
+          <Button 
+          onClick={() => {
+            refetch({
+              where: {
+                requestJson_CONTAINS: val
+              },
+              requestsConnectionWhere2: {
+                requestJson_CONTAINS: val
+              },
+              options: { limit: 20, offset: 0 }
+            });
+          }}>
+          </Button>
         </Form>
+
       </InputGroup>
       <InfiniteLoader
         isRowLoaded={isRowLoaded}
@@ -169,80 +182,80 @@ const RecentDeliveriesObserverable = observer(() => {
               return (
                 <Column
                   headerRenderer={headerRenderer}
-                  cellRenderer={({
-                    cellData,
-                    columnIndex = null,
-                    rowIndex
-                  }) => {
-                    return cellRenderer({ cellData });
-                  }}
                   headerStyle={{ display: "inline-block" }}
                   style={{ display: "inline-block" }}
                   label={col.label}
-                  dataKey={`${col.dataKey}`}
+                  dataKey={`${col.dataKey}`} 
+                  cellRenderer={col.cellRenderer}
                   width={1100 / 8}
                 />
               );
             })}
+            
           </Table>
         )}
       </InfiniteLoader>
     </Row>
   );
-});
-
+};
+// on "edit button click" enter editing mode
+// make "edit button" cli
+// cellrenderer gets rowData (sample properties)
 const RecentDeliveriesColumns = [
   {
-    selector: (d: Request) => d.igoRequestId,
+    label: "Edit",
+    cellRenderer: (arg)=>{console.log(arg); return (<Button onClick={()=>{
+      // call setState --> puts us in editing mode
+      // introducing a new state property (state = editingSampleId)
+      // if "editingSampleId" defined then render the form for editing (app re-renders, shows form)
+      // if "editingSampleId" undefined then app knows not to show the other stuff, maybe shows in modal 
+      // for now let's put form at the bottom 
+      alert(arg.rowData.igoRequestId)
+    }}>edit</Button>)}
+  },
+  {
     dataKey: "igoRequestId",
     label: "IGO Request ID",
     sortable: true,
-    filterable: true
+    filterable: true,
   },
   {
-    selector: (d: Request) => d.igoProjectId,
     dataKey: "igoProjectId",
     label: "IGO Project ID",
     sortable: true,
     filterable: true
   },
   {
-    selector: (d: Request) => d.projectManagerName,
     dataKey: "projectManagerName",
     label: "Project Manager Name",
     sortable: true,
     filterable: true
   },
   {
-    selector: (d: Request) => d.investigatorName,
     dataKey: "investigatorName",
     label: "Investigator Name",
     sortable: true,
     filterable: true
   },
   {
-    selector: (d: Request) => d.investigatorEmail,
     dataKey: "investigatorEmail",
     label: "Investigator Email",
     sortable: true,
     filterable: true
   },
   {
-    selector: (d: Request) => d.dataAnalystName,
     dataKey: "dataAnalystName",
     label: "Data Analyst Name",
     sortable: true,
     filterable: true
   },
   {
-    selector: (d: Request) => d.dataAnalystEmail,
     dataKey: "dataAnalystEmail",
     label: "Data Analyst Email",
     sortable: true,
     filterable: true
   },
   {
-    selector: (d: Request) => d.genePanel,
     dataKey: "genePanel",
     label: "Gene Panel",
     sortable: true,
