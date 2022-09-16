9a55c95cc17a81bbcc10cc03f57494dcded3f1a0
shalabhsuman <shalabhsuman@gmail.com>
2022-09-16 14:34:32 -0400

    changes for export functionality

--- src/pages/recentDeliveries/helpers.tsx
+++ src/pages/recentDeliveries/helpers.tsx
@@ -2,7 +2,74 @@ import {Edit} from "@material-ui/icons";
 import {Button} from "react-bootstrap";
 import React from "react";
 
-export function buildRequestTableColumns(navigate:any){
+type ColumnDefinition = {
+    dataKey:String,
+    label:String,
+    sortable:Boolean,
+    filterable:Boolean,
+    width?:Number,
+    headerRender:()=>any,
+    cellRenderer:()=>any
+}
+
+export const StaticTableColumns:ColumnDefinition[] = [
+    {
+        dataKey: "igoRequestId",
+        label: "IGO Request ID",
+        sortable: true,
+        filterable: true
+    },
+    {
+        dataKey: "igoProjectId",
+        label: "IGO Project ID",
+        sortable: true,
+        filterable: true
+    },
+    {
+        dataKey: "projectManagerName",
+        label: "Project Manager Name",
+        sortable: true,
+        filterable: true,
+        width: 200
+    },
+    {
+        dataKey: "investigatorName",
+        label: "Investigator Name",
+        sortable: true,
+        filterable: true,
+        width: 200
+    },
+    {
+        dataKey: "investigatorEmail",
+        label: "Investigator Email",
+        sortable: true,
+        filterable: true,
+        width: 200
+    },
+    {
+        dataKey: "dataAnalystName",
+        label: "Data Analyst Name",
+        sortable: true,
+        filterable: true,
+        width: 200
+    },
+    {
+        dataKey: "dataAnalystEmail",
+        label: "Data Analyst Email",
+        sortable: true,
+        filterable: true,
+        width: 200
+    },
+    {
+        dataKey: "genePanel",
+        label: "Gene Panel",
+        sortable: true,
+        filterable: true
+    }
+];
+
+
+export function buildRequestTableColumns(navigate:any):ColumnDefinition[]{
 
     return [
         {
@@ -23,59 +90,9 @@ export function buildRequestTableColumns(navigate:any){
                 );
             }
         },
-        {
-            dataKey: "igoRequestId",
-            label: "IGO Request ID",
-            sortable: true,
-            filterable: true
-        },
-        {
-            dataKey: "igoProjectId",
-            label: "IGO Project ID",
-            sortable: true,
-            filterable: true
-        },
-        {
-            dataKey: "projectManagerName",
-            label: "Project Manager Name",
-            sortable: true,
-            filterable: true,
-            width: 200
-        },
-        {
-            dataKey: "investigatorName",
-            label: "Investigator Name",
-            sortable: true,
-            filterable: true,
-            width: 200
-        },
-        {
-            dataKey: "investigatorEmail",
-            label: "Investigator Email",
-            sortable: true,
-            filterable: true,
-            width: 200
-        },
-        {
-            dataKey: "dataAnalystName",
-            label: "Data Analyst Name",
-            sortable: true,
-            filterable: true,
-            width: 200
-        },
-        {
-            dataKey: "dataAnalystEmail",
-            label: "Data Analyst Email",
-            sortable: true,
-            filterable: true,
-            width: 200
-        },
-        {
-            dataKey: "genePanel",
-            label: "Gene Panel",
-            sortable: true,
-            filterable: true
-        }
+
+        ...StaticTableColumns
+       
     ];
 
 
