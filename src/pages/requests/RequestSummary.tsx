6b77ba6cb4ad96befc5996cad9d191b439948e40
alisman <lisman.aaron@gmail.com>
2022-10-21 13:43:00 -0400

    Fix bug where request page reloads when samples are requeried

--- src/pages/requests/RequestSummary.tsx
+++ src/pages/requests/RequestSummary.tsx
@@ -21,154 +21,152 @@ interface IRequestSummaryProps {
   params: Readonly<Params<string>>;
 }
 
-const RequestSummary: FunctionComponent<IRequestSummaryProps> = observer(
-  ({ params }) => {
-    const {
-      loading,
-      error,
-      data,
-      refetch,
-      fetchMore
-    } = useRequestWithSamplesQuery({
-      variables: {
-        where: {
-          igoRequestId: params.requestId
-        },
-        options: {
-          offset: 0,
-          limit: undefined
-        }
+const RequestSummary: FunctionComponent<IRequestSummaryProps> = ({
+  params
+}) => {
+  const {
+    loading,
+    error,
+    data,
+    refetch,
+    fetchMore
+  } = useRequestWithSamplesQuery({
+    variables: {
+      where: {
+        igoRequestId: params.requestId
+      },
+      options: {
+        offset: 0,
+        limit: undefined
       }
-    });
+    },
+    fetchPolicy: "no-cache"
+  });
 
-    const [val, setVal] = useState("");
-    const [showDownloadModal, setShowDownloadModal] = useState(false);
-    const [typingTimeout, setTypingTimeout] = useState<any>(null);
-    const [prom, setProm] = useState<any>(Promise.resolve());
+  const [val, setVal] = useState("");
+  const [showDownloadModal, setShowDownloadModal] = useState(false);
+  const [typingTimeout, setTypingTimeout] = useState<any>(null);
+  const [prom, setProm] = useState<any>(Promise.resolve());
 
-    if (loading)
-      return (
-        <div className={"centralSpinner"}>
-          <Spinner fadeIn={"none"} color={"lightblue"} name="ball-grid-pulse" />
-        </div>
-      );
-
-    if (error)
-      return <Row>Error loading request details / request samples</Row>;
-
-    const request = data!.requests[0];
-    const samples = request.hasSampleSamples;
-    const metadataList = samples.map(item => item.hasMetadataSampleMetadata[0]);
-
-    function rowGetter({ index }: Index) {
-      return request.hasSampleSamples[index].hasMetadataSampleMetadata[0];
-    }
-
-    const sampleTable = (
-      <AutoSizer>
-        {({ width }) => (
-          <Table
-            className="table"
-            width={width}
-            height={450}
-            headerHeight={50}
-            rowHeight={40}
-            rowCount={request.hasSampleSamples.length}
-            rowGetter={rowGetter}
-          >
-            {SampleDetailsColumns.map(col => {
-              return (
-                <Column
-                  label={col.label}
-                  dataKey={`${col.dataKey}`}
-                  width={width / SampleDetailsColumns.length}
-                />
-              );
-            })}
-          </Table>
-        )}
-      </AutoSizer>
-    );
-
-    const stringFields: any[] = [];
-
-    _.forEach(request, (val: any, key: string) => {
-      if (typeof val === "string") {
-        stringFields.push(
-          <tr>
-            <td>{key}</td>
-            <td>{val}</td>
-          </tr>
-        );
-      }
-    });
-
+  if (loading)
     return (
-      <>
-        {showDownloadModal && (
-          <DownloadModal
-            loader={() => {
-              return Promise.resolve(
-                CSVFormulate(metadataList, SampleDetailsColumns)
-              );
-            }}
-            onComplete={() => {
-              setShowDownloadModal(false);
-            }}
-            exportFilename={
-              "request_" + data?.requests[0].igoRequestId + ".tsv"
-            }
-          />
-        )}
-
-        <Col className={"text-end"}>
-          <Form.Control
-            className={"d-inline-block"}
-            style={{ width: "300px" }}
-            type="search"
-            placeholder="Search Samples"
-            aria-label="Search"
-            value={val}
-            onInput={event => {
-              const value = event.currentTarget.value;
-
-              if (value !== null) {
-                setVal(value);
-              }
-
-              if (typingTimeout) {
-                clearTimeout(typingTimeout);
-              }
-
-              prom.then(() => {
-                const to = setTimeout(() => {
-                  const rf = refetch({
-                    hasSampleSamplesWhere2: {
-                      sampleClass: _.isEmpty(value) ? undefined : value
-                    }
-                  });
-                  setProm(rf);
-                }, 500);
-                setTypingTimeout(to);
-              });
-            }}
-          />
-        </Col>
-
-        <Col className={"text-end"}>
-          <Button
-            onClick={() => {
-              setShowDownloadModal(true);
-            }}
-          >
-            Generate Sample Report
-          </Button>
-        </Col>
-
-        <div style={{ height: 540 }}>{sampleTable}</div>
-      </>
+      <div className={"centralSpinner"}>
+        <Spinner fadeIn={"none"} color={"lightblue"} name="ball-grid-pulse" />
+      </div>
     );
+
+  if (error) return <Row>Error loading request details / request samples</Row>;
+
+  const request = data!.requests[0];
+  const samples = request.hasSampleSamples;
+  const metadataList = samples.map(item => item.hasMetadataSampleMetadata[0]);
+
+  function rowGetter({ index }: Index) {
+    return request.hasSampleSamples[index].hasMetadataSampleMetadata[0];
   }
-);
+
+  const sampleTable = (
+    <AutoSizer>
+      {({ width }) => (
+        <Table
+          className="table"
+          width={width}
+          height={450}
+          headerHeight={50}
+          rowHeight={40}
+          rowCount={request.hasSampleSamples.length}
+          rowGetter={rowGetter}
+        >
+          {SampleDetailsColumns.map(col => {
+            return (
+              <Column
+                label={col.label}
+                dataKey={`${col.dataKey}`}
+                width={width / SampleDetailsColumns.length}
+              />
+            );
+          })}
+        </Table>
+      )}
+    </AutoSizer>
+  );
+
+  const stringFields: any[] = [];
+
+  _.forEach(request, (val: any, key: string) => {
+    if (typeof val === "string") {
+      stringFields.push(
+        <tr>
+          <td>{key}</td>
+          <td>{val}</td>
+        </tr>
+      );
+    }
+  });
+
+  return (
+    <>
+      {showDownloadModal && (
+        <DownloadModal
+          loader={() => {
+            return Promise.resolve(
+              CSVFormulate(metadataList, SampleDetailsColumns)
+            );
+          }}
+          onComplete={() => {
+            setShowDownloadModal(false);
+          }}
+          exportFilename={"request_" + data?.requests[0].igoRequestId + ".tsv"}
+        />
+      )}
+
+      <Col className={"text-end"}>
+        <Form.Control
+          className={"d-inline-block"}
+          style={{ width: "300px" }}
+          type="search"
+          placeholder="Search Samples"
+          aria-label="Search"
+          value={val}
+          onInput={event => {
+            const value = event.currentTarget.value;
+
+            if (value !== null) {
+              setVal(value);
+            }
+
+            if (typingTimeout) {
+              clearTimeout(typingTimeout);
+            }
+
+            prom.then(() => {
+              const to = setTimeout(() => {
+                const rf = refetch({
+                  hasSampleSamplesWhere2: {
+                    sampleClass: _.isEmpty(value) ? undefined : value
+                  }
+                });
+                setProm(rf);
+              }, 500);
+              setTypingTimeout(to);
+            });
+          }}
+        />
+      </Col>
+
+      <Col className={"text-end"}>
+        <Button
+          onClick={() => {
+            setShowDownloadModal(true);
+          }}
+        >
+          Generate Sample Report
+        </Button>
+      </Col>
+
+      <div style={{ height: 540 }}>{sampleTable}</div>
+    </>
+  );
+};
 
 export { RequestSummary };
