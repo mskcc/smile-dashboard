a62929bc21dda985d8e32cb2cd678b7398daf79f
Jennifer Flores <jeflores@mills.edu>
2022-11-21 13:50:26 -0800

    Clean up

--- src/pages/requests/helpers.tsx
+++ src/pages/requests/helpers.tsx
@@ -1,4 +1,3 @@
-import { Edit } from "@material-ui/icons";
 import { ColDef } from "ag-grid-community";
 import { Button } from "react-bootstrap";
 
@@ -13,154 +12,6 @@ export type ColumnDefinition = {
   cellDataGetter?: (arg: any) => any;
 };
 
-export const RequestsListColumns: ColumnDefinition[] = [
-  {
-    dataKey: "igoRequestId",
-    label: "IGO Request ID",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "igoProjectId",
-    label: "IGO Project ID",
-    sortable: true,
-    filterable: true,
-    width: 175
-  },
-  {
-    dataKey: "hasSampleSamplesConnection",
-    label: "# Samples",
-    sortable: true,
-    cellDataGetter: function({ dataKey, rowData }) {
-      return rowData[dataKey]?.totalCount;
-    }
-  },
-  {
-    dataKey: "projectManagerName",
-    label: "Project Manager Name",
-    sortable: true,
-    filterable: true,
-    width: 175
-  },
-  {
-    dataKey: "investigatorName",
-    label: "Investigator Name",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "investigatorEmail",
-    label: "Investigator Email",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "piEmail",
-    label: "PI Email",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "dataAnalystName",
-    label: "Data Analyst Name",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "dataAnalystEmail",
-    label: "Data Analyst Email",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "genePanel",
-    label: "Gene Panel",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "labHeadName",
-    label: "Lab Head Name",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "labHeadEmail",
-    label: "Lab Head Email",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "qcAccessEmails",
-    label: "QC Access Emails",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "dataAccessEmails",
-    label: "Data Access Emails",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "bicAnalysis",
-    label: "BIC Analysis",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "isCmoRequest",
-    label: "CMO Request?",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "otherContactEmails",
-    label: "Other Contact Emails",
-    sortable: true,
-    filterable: true,
-    width: 200
-  }
-];
-
-export function oldBuildRequestTableColumns(navigate: any): ColumnDefinition[] {
-  return [
-    {
-      headerRender: () => {
-        return <Edit />;
-      },
-      cellRenderer: arg => {
-        return (
-          <Button
-            variant="outline-secondary"
-            size="sm"
-            onClick={() => {
-              navigate("./" + arg.rowData.igoRequestId);
-            }}
-          >
-            View
-          </Button>
-        );
-      }
-    },
-
-    ...RequestsListColumns
-  ];
-}
-
 export function buildRequestTableColumns(navigate: any): ColDef[] {
   return [
     {
@@ -172,7 +23,7 @@ export function buildRequestTableColumns(navigate: any): ColDef[] {
             size="sm"
             onClick={(function(...args) {
               return function() {
-                // console.log(data.hasSampleSamplesConnection);
+                // TO DO: navigate to RequestSummary Page
               };
             })()}
           >
@@ -181,11 +32,11 @@ export function buildRequestTableColumns(navigate: any): ColDef[] {
         );
       }
     },
-    ...newColumns
+    ...RequestsListColumns
   ];
 }
 
-export const newColumns: ColDef[] = [
+export const RequestsListColumns: ColDef[] = [
   {
     field: "igoRequestId",
     headerName: "IGO Request ID",
@@ -289,93 +140,6 @@ export const newColumns: ColDef[] = [
     field: "otherContactEmails",
     headerName: "Other Contact Emails",
     sortable: true,
-    width: 200
-  }
-];
-
-export const oldSampleDetailsColumns: ColumnDefinition[] = [
-  {
-    dataKey: "cmoSampleName",
-    label: "CMO Sample Name",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "cmoPatientId",
-    label: "CMO Patient ID",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "investigatorSampleId",
-    label: "Investigator Sample ID",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "primaryId",
-    label: "Primary ID",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "preservation",
-    label: "Preservation",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "tumorOrNormal",
-    label: "Tumor Or Normal",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "sampleClass",
-    label: "Sample Class",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "oncotreeCode",
-    label: "Oncotree Code",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "collectionYear",
-    label: "Collection Year",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "sampleOrigin",
-    label: "Sample Origin",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "tissueLocation",
-    label: "Tissue Location",
-    sortable: true,
-    filterable: true,
-    width: 200
-  },
-  {
-    dataKey: "sex",
-    label: "Sex",
-    sortable: true,
-    filterable: true,
     width: 200
   }
 ];
