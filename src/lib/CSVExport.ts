88a587c5e9f9732832b79d74133a57d395c7974c
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2022-11-29 15:56:00 -0500

    AgGrid integration and server side data filtering
    These changes introduce AgGrid community and enterprise dependencies for
    our new data table views for requests and request samples.
    
    Additional changes:
    - added a search bar for filtering requests and samples
    - expanded filter criteria for requests and samples
    - minor styling changes to allow width of tables being displayed
     to change with size of browser window
    
    Bug fixes:
    - fixed bug where request page reloads when samples are requeried
    - fixed the export button to include sample counts column
    - fixed inaccurate counts of filter state for requests and samples
    
    Co-authored-by: alisman
    Co-authored-by: jennnyyy
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- src/lib/CSVExport.ts
+++ src/lib/CSVExport.ts
@@ -1,8 +1,24 @@
-export function CSVFormulate(requests, columnDefinitions) {
+import { ColumnDefinition } from "../pages/requests/helpers";
+
+export function CSVFormulate(
+  requests: any[],
+  columnDefinitions: ColumnDefinition[]
+) {
   const csvString = [
     columnDefinitions.map(item => item.label).join("\t"),
     ...requests
-      .map(req => columnDefinitions.map(col => req[col.dataKey!]))
+      .map(req =>
+        columnDefinitions.map(col => {
+          if (col.cellDataGetter) {
+            return col.cellDataGetter({
+              dataKey: col.dataKey,
+              rowData: req
+            });
+          } else {
+            return req[col.dataKey!];
+          }
+        })
+      )
       .map(e => e.join("\t"))
   ].join("\n");
   return csvString;
--- src/pages/requests/RequestSummary.tsx
+++ src/pages/requests/RequestSummary.tsx
@@ -1,83 +1,75 @@
 import { useRequestWithSamplesQuery } from "../../generated/graphql";
-import { AutoSizer, Column, InfiniteLoader, Table } from "react-virtualized";
-import { Button, Col, Row } from "react-bootstrap";
-import { observer } from "mobx-react";
-import "react-virtualized/styles.css";
-import _ from "lodash";
-import { useState } from "react";
+import { AutoSizer, Index } from "react-virtualized";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import _, { sample } from "lodash";
+import classNames from "classnames";
+import { FunctionComponent } from "react";
 import { DownloadModal } from "../../components/DownloadModal";
 import { CSVFormulate } from "../../lib/CSVExport";
 import { SampleDetailsColumns } from "./helpers";
+import { Params } from "react-router-dom";
+import Spinner from "react-spinkit";
+import { AgGridReact } from "ag-grid-react";
+import { useState } from "react";
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import "ag-grid-enterprise";
 
-const RequestSummary = observer(({ props }) => {
-  const { loading, error, data, fetchMore } = useRequestWithSamplesQuery({
+interface IRequestSummaryProps {
+  params: Readonly<Params<string>>;
+  height: number;
+}
+
+const RequestSummary: FunctionComponent<IRequestSummaryProps> = ({
+  params,
+  height
+}) => {
+  const {
+    loading,
+    error,
+    data,
+    refetch,
+    fetchMore
+  } = useRequestWithSamplesQuery({
     variables: {
       where: {
-        igoRequestId: props.requestId
+        igoRequestId: params.requestId
       },
       options: {
         offset: 0,
         limit: undefined
       }
-    }
+    },
+    fetchPolicy: "no-cache"
   });
 
+  const [val, setVal] = useState("");
   const [showDownloadModal, setShowDownloadModal] = useState(false);
+  const [typingTimeout, setTypingTimeout] = useState<any>(null);
+  const [prom, setProm] = useState<any>(Promise.resolve());
 
-  if (loading) return <Row />;
+  if (loading)
+    return (
+      <div className={"centralSpinner"}>
+        <Spinner fadeIn={"none"} color={"lightblue"} name="ball-grid-pulse" />
+      </div>
+    );
+
   if (error) return <Row>Error loading request details / request samples</Row>;
 
   const request = data!.requests[0];
   const samples = request.hasSampleSamples;
   const metadataList = samples.map(item => item.hasMetadataSampleMetadata[0]);
 
-  function rowGetter({ index }) {
-    return request.hasSampleSamples[index].hasMetadataSampleMetadata[0];
+  function rowGetter({ index }: Index) {
+    const s = request.hasSampleSamples[index];
+    const sm = request.hasSampleSamples[index].hasMetadataSampleMetadata[0];
+    return sm || {};
   }
-
-  const sampleTable = (
-    <AutoSizer>
-      {({ width }) => (
-        <Table
-          className="table"
-          width={width}
-          height={450}
-          headerHeight={50}
-          rowHeight={40}
-          rowCount={request.hasSampleSamples.length}
-          rowGetter={rowGetter}
-        >
-          {SampleDetailsColumns.map(col => {
-            return (
-              <Column
-                label={col.label}
-                dataKey={`${col.dataKey}`}
-                width={width / SampleDetailsColumns.length}
-              />
-            );
-          })}
-        </Table>
-      )}
-    </AutoSizer>
-  );
 
   const stringFields: any[] = [];
 
-  function loadAllRows(fetchMore: any, filter: string) {
-    return () => {
-      return fetchMore({
-        variables: {
-          where: {},
-          options: {
-            offset: 0,
-            limit: undefined
-          }
-        }
-      });
-    };
-  }
-
-  _.forEach(request, (val, key) => {
+  _.forEach(request, (val: any, key: string) => {
     if (typeof val === "string") {
       stringFields.push(
         <tr>
@@ -88,14 +80,16 @@ const RequestSummary = observer(({ props }) => {
     }
   });
 
+  const remoteCount = request.hasSampleSamples.length;
+
   return (
     <>
       {showDownloadModal && (
         <DownloadModal
           loader={() => {
-            return loadAllRows(fetchMore, "")().then(({ data }) => {
-              return CSVFormulate(metadataList, SampleDetailsColumns);
-            });
+            return Promise.resolve(
+              CSVFormulate(metadataList, SampleDetailsColumns)
+            );
           }}
           onComplete={() => {
             setShowDownloadModal(false);
@@ -103,20 +97,76 @@ const RequestSummary = observer(({ props }) => {
           exportFilename={"request_" + data?.requests[0].igoRequestId + ".tsv"}
         />
       )}
+      <Row
+        className={classNames(
+          "d-flex justify-content-between align-items-center"
+        )}
+      >
+        <Col></Col>
+        <Col className={"text-end"}>
+          <Form.Control
+            className={"d-inline-block"}
+            style={{ width: "300px" }}
+            type="search"
+            placeholder="Search Samples"
+            aria-label="Search"
+            value={val}
+            onInput={event => {
+              const value = event.currentTarget.value;
 
-      <Col className={"text-end"}>
-        <Button
-          onClick={() => {
-            setShowDownloadModal(true);
-          }}
-        >
-          Generate Sample Report
-        </Button>
-      </Col>
+              if (value !== null) {
+                setVal(value);
+              }
 
-      <div style={{ height: 540 }}>{sampleTable}</div>
+              if (typingTimeout) {
+                clearTimeout(typingTimeout);
+              }
+
+              prom.then(() => {
+                const to = setTimeout(() => {
+                  const rf = refetch({
+                    hasSampleSamplesWhere2: {
+                      sampleClass_CONTAINS: value
+                    },
+                    hasSamplesConnectionWhere2: {
+                      node: { sampleClass_CONTAINS: value }
+                    }
+                  });
+                  setProm(rf);
+                }, 500);
+                setTypingTimeout(to);
+              });
+            }}
+          />
+        </Col>
+
+        <Col className={"text-start"}>{remoteCount} matching requests</Col>
+
+        <Col className={"text-end"}>
+          <Button
+            onClick={() => {
+              setShowDownloadModal(true);
+            }}
+          >
+            Generate Sample Report
+          </Button>
+        </Col>
+      </Row>
+      <AutoSizer>
+        {({ width }) => (
+          <div
+            className="ag-theme-alpine"
+            style={{ height: height, width: width }}
+          >
+            <AgGridReact
+              columnDefs={SampleDetailsColumns}
+              rowData={metadataList}
+            />
+          </div>
+        )}
+      </AutoSizer>
     </>
   );
-});
+};
 
 export { RequestSummary };
