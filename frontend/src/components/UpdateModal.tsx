2740d0d4dbe56b4a756606e552cab7d0dc65202b
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-21 11:34:54 -0500

    Display a summary of changes in the Submit Updates confirmation modal

--- frontend/src/components/UpdateModal.tsx
+++ frontend/src/components/UpdateModal.tsx
@@ -1,17 +1,28 @@
-import React, { FunctionComponent } from "react";
+import { useState, FunctionComponent, useEffect } from "react";
 import { Button } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
+import { AgGridReact } from "ag-grid-react";
 import "./UpdateModal.css";
-
-/* TODOs by priority
-- Store cell updates in the changes object
-- Submit Updates button to make a Mutation call to GraphQL server
-*/
+import "ag-grid-community/styles/ag-grid.css";
+import "ag-grid-community/styles/ag-theme-alpine.css";
+import { CellChange } from "../pages/requests/helpers";
 
 export const UpdateModal: FunctionComponent<{
-  changes: object;
+  changes: CellChange[];
   onHide: () => void;
 }> = ({ changes, onHide }) => {
+  const [rowData, setRowData] = useState(changes);
+  const [columnDefs] = useState([
+    { field: "primaryId" },
+    { field: "field" },
+    { field: "oldValue" },
+    { field: "newValue" },
+  ]);
+
+  useEffect(() => {
+    setRowData(changes);
+  }, [changes]);
+
   return (
     <Modal
       show={true}
@@ -28,7 +39,9 @@ export const UpdateModal: FunctionComponent<{
 
       <Modal.Body>
         <p>Are you sure you want to submit the following changes?</p>
-        <pre>{JSON.stringify(changes, null, 2)}</pre>
+        <div className="ag-theme-alpine" style={{ height: 350 }}>
+          <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+        </div>
       </Modal.Body>
 
       <Modal.Footer>
