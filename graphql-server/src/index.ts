bb4ae6540877f8717cd14af534dd3ad603f09d0a
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2023-02-21 10:20:10 -0500

    Working publisher and updateSamples mutation
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- graphql-server/src/index.ts
+++ graphql-server/src/index.ts
@@ -1,13 +1,8 @@
 import { Express } from "express";
-import {
-  UpdateSamplesMutation,
-  UpdateSamplesMutationVariables,
-} from "./generated/graphql";
-import { resolvers } from "./resolvers";
 
 const path = require("path");
 const { Neo4jGraphQL } = require("@neo4j/graphql");
-const { ApolloServer, gql } = require("apollo-server-express");
+const { ApolloServer } = require("apollo-server-express");
 const { toGraphQLTypeDefs } = require("@neo4j/introspector");
 const neo4j = require("neo4j-driver");
 const { connect, StringCodec } = require("nats");
@@ -15,6 +10,7 @@ var PropertiesReader = require("properties-reader");
 var properties = new PropertiesReader(
   path.resolve(__dirname, "../dist/env/application.properties")
 );
+const request = require("request");
 const http = require("http");
 const bodyParser = require("body-parser");
 const express = require("express");
@@ -38,18 +34,12 @@ const nats_ca_pem = properties.get("conn.nats_ca_pem");
 const nats_url = properties.get("conn.nats_url");
 
 // pub-sub topics
-const pub_validate_request_update = properties.get(
-  "topics.pub_validate_igo_request_update"
-);
 const pub_validate_sample_update = properties.get(
   "topics.pub_validate_igo_sample_update"
 );
-const sub_cmo_request_update = properties.get(
-  "topics.sub_consumers_cmo_request_update"
-);
-const sub_cmo_sample_update = properties.get(
-  "topics.sub_consumers_cmo_sample_update"
-);
+
+// smile sample endpoint
+const smile_sample_endpoint = properties.get("smile.smile_sample_endpoint");
 
 const sc = StringCodec();
 
@@ -67,16 +57,15 @@ const natsConnProperties = {
   tls: tlsOptions,
 };
 
-var nc = null;
-async function establishConnection() {
+const natsConn = connect(natsConnProperties);
+
+async function publishNatsMessage(topic: String, message: String) {
   try {
     const natsConn = await connect(natsConnProperties);
-    nc = natsConn;
     console.log("Connected to server: ");
     console.log(natsConn.getServer());
-
     // setting up subscribers
-    const sub_req_update = natsConn.subscribe(sub_cmo_request_update);
+    // const sub_req_update = natsConn.subscribe(sub_cmo_request_update);
     // (async () => {
     //   for await (const m of sub_req_update) {
     //     console.log(`[${sub_req_update.getProcessed()}]: ${sc.decode(m.data)}`);
@@ -94,11 +83,8 @@ async function establishConnection() {
 
     // TODO: set up publisher to publish on sample/request update topics
     // const message = JSON.stringify({message});
-    // console.log("publishing message", message);
-    // natsConn.publish(
-    //   "TOPIC TO PUBLISH TO",
-    //   sc.encode(message)
-    // );
+    console.log("publishing message: ", message);
+    natsConn.publish(topic, sc.encode(message));
   } catch (err) {
     console.log(
       `error connecting to ${JSON.stringify(natsConnProperties)}`,
@@ -116,8 +102,6 @@ const sessionFactory = () =>
   driver.session({ defaultAccessMode: neo4j.session.WRITE });
 
 async function main() {
-  establishConnection(); // for nats only
-
   const app: Express = express();
   app.use(express.static(path.resolve(__dirname, "../build")));
   app.use(bodyParser.urlencoded({ extended: true }));
@@ -129,6 +113,7 @@ async function main() {
 
   const httpServer = http.createServer(app);
   const typeDefs = await toGraphQLTypeDefs(sessionFactory, false);
+  const ogm = new OGM({ typeDefs, driver });
   const neoSchema = new Neo4jGraphQL({
     typeDefs,
     driver,
@@ -136,15 +121,44 @@ async function main() {
       ApolloServerPluginDrainHttpServer({ httpServer }),
       ApolloServerPluginLandingPageLocalDefault({ embed: true }),
     ],
-    resolvers: resolvers,
-  });
+    resolvers: {
+      Mutation: {
+        updateSamples: async (_source: any, { where, update }: any) => {
+          request(
+            smile_sample_endpoint + where.smileSampleId,
+            { json: true },
+            (err: any, res: any) => {
+              if (err) {
+                return console.log(err);
+              }
+              // set revisable to false for sample
+              let sample = ogm.model("Sample");
+              sample.find({ where: { smileSampleId: where.smileSampleId } });
+              // get latest sample metadata and apply updates provided through mutation
+              let sampleData = res.body;
+              const smdataupdates =
+                update.hasMetadataSampleMetadata[0].update.node;
+              Object.keys(smdataupdates).forEach((key: string) => {
+                sampleData[key] = smdataupdates[key];
+              });
 
-  const ogm = new OGM({ typeDefs, driver });
+              // publish sample update to nats server
+              console.log("publishing message", JSON.stringify(sampleData));
+              publishNatsMessage(
+                pub_validate_sample_update,
+                JSON.stringify(sampleData)
+              );
+              return { samples: [sampleData] };
+            }
+          );
+        },
+      },
+    },
+  });
 
   Promise.all([neoSchema.getSchema(), ogm.init()]).then(async ([schema]) => {
     const server = new ApolloServer({
       schema,
-      //    context: ({ req } : any) => ({ req })
     });
     await server.start();
     server.applyMiddleware({ app });
@@ -156,3 +170,10 @@ async function main() {
 }
 
 main();
+function useState(arg0: never[]): [any, any] {
+  throw new Error("Function not implemented.");
+}
+
+function useEffect(arg0: () => void, arg1: never[]) {
+  throw new Error("Function not implemented.");
+}
