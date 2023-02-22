6054676fb81e60cac5d08fe1a11b59d8b8091ce4
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-02-22 14:58:00 -0500

    Group changes by Primary Id in Submit Updates modal's summary of changes table

--- frontend/src/components/UpdateModal.tsx
+++ frontend/src/components/UpdateModal.tsx
@@ -1,7 +1,8 @@
-import { useState, FunctionComponent, useEffect } from "react";
+import { useState, FunctionComponent, useEffect, useMemo } from "react";
 import { Button } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import { AgGridReact } from "ag-grid-react";
+import "ag-grid-enterprise";
 import "./UpdateModal.css";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-alpine.css";
@@ -13,7 +14,7 @@ export const UpdateModal: FunctionComponent<{
 }> = ({ changes, onHide }) => {
   const [rowData, setRowData] = useState(changes);
   const [columnDefs] = useState([
-    { field: "primaryId" },
+    { field: "primaryId", rowGroup: true, hide: true },
     { field: "field" },
     { field: "oldValue" },
     { field: "newValue" },
@@ -22,6 +23,13 @@ export const UpdateModal: FunctionComponent<{
   useEffect(() => {
     setRowData(changes);
   }, [changes]);
+
+  const autoGroupColumnDef = useMemo(() => {
+    return {
+      headerName: "Primary Id",
+      field: "primaryId",
+    };
+  }, []);
 
   return (
     <Modal
@@ -40,7 +48,13 @@ export const UpdateModal: FunctionComponent<{
       <Modal.Body>
         <p>Are you sure you want to submit the following changes?</p>
         <div className="ag-theme-alpine" style={{ height: 350 }}>
-          <AgGridReact rowData={rowData} columnDefs={columnDefs}></AgGridReact>
+          <AgGridReact
+            rowData={rowData}
+            columnDefs={columnDefs}
+            groupRemoveSingleChildren={true}
+            autoGroupColumnDef={autoGroupColumnDef}
+            groupDefaultExpanded={1}
+          ></AgGridReact>
         </div>
       </Modal.Body>
 
