f5a7deff47d9ed7d1cc393a0efbae6270a5fdb90
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-03-15 13:55:47 -0400

    Add a search form

--- frontend/src/pages/patients/PatientsPage.tsx
+++ frontend/src/pages/patients/PatientsPage.tsx
@@ -1,11 +1,16 @@
-import React, { FunctionComponent } from "react";
-import { Col, Container, Row } from "react-bootstrap";
+import React, { FunctionComponent, useState } from "react";
+import { Col, Container, Form, Row } from "react-bootstrap";
 
 export const PatientsPage: FunctionComponent = () => {
   return <Patients />;
 };
 
 const Patients: FunctionComponent = () => {
+  const [val, setVal] = useState("");
+  const [typingTimeout, setTypingTimeout] = useState<ReturnType<
+    typeof setTimeout
+  > | null>(null);
+
   return (
     <Container fluid>
       <Row className="pagetitle">
@@ -21,6 +26,26 @@ const Patients: FunctionComponent = () => {
           <h1>Patients</h1>
         </Col>
       </Row>
+      <Form.Control
+        className={"d-inline-block"}
+        style={{ width: "300px" }}
+        type="search"
+        placeholder="Search by CMO Patient ID"
+        aria-label="Search"
+        defaultValue={val}
+        onInput={(event) => {
+          const value = event.currentTarget.value;
+
+          if (typingTimeout) {
+            clearTimeout(typingTimeout);
+          }
+
+          const to = setTimeout(() => {
+            setVal(value);
+          }, 500);
+          setTypingTimeout(to);
+        }}
+      />
     </Container>
   );
 };
