9518463b594d4c0400e83d88881e18fc9f86b499
Divya Madala <71040191+divyamadala30@users.noreply.github.com>
2023-05-09 15:12:09 -0400

    working state report pop-up
    Signed-off-by: Divya Madala <71040191+divyamadala30@users.noreply.github.com>

--- frontend/src/pages/requests/StatusToolTip.tsx
+++ frontend/src/pages/requests/StatusToolTip.tsx
@@ -1,23 +1,47 @@
-import { ITooltipComp, ITooltipParams } from "ag-grid-community";
+import { ITooltipParams } from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
+import { useState } from "react";
 
-export class StatusTooltip implements ITooltipComp {
-  eGui: any;
-  init(params: ITooltipParams & { color: string }) {
-    const eGui = (this.eGui = document.createElement("div"));
-    const color = params.color || "white";
-    const data = params.api!.getDisplayedRowAtIndex(params.rowIndex!)!.data;
+export const StatusTooltip = (props: ITooltipParams) => {
+  const data = props.api!.getDisplayedRowAtIndex(props.rowIndex!)!.data;
 
-    eGui.classList.add("custom-tooltip");
-    //@ts-ignore
-    eGui.style["background-color"] = color;
-    eGui.innerHTML = `
-            <p>
-                <span class"name">${data?.["hasStatusStatuses"][0].validationStatus}</span>
-            </p>
-        `;
+  const [columnDefs] = useState([
+    { field: "fieldName", headerName: "Field Name" },
+    {
+      field: "reason",
+      headerName: "Reason",
+      wrapText: true,
+      autoHeight: true,
+      flex: 2,
+    },
+  ]);
+
+  const validationReportMap = new Map(
+    Object.entries(JSON.parse(data?.hasStatusStatuses[0].validationReport))
+  );
+  const validationReportList: any[] | null | undefined = [];
+
+  validationReportMap.forEach(function (value, key) {
+    validationReportList.push({ fieldName: key, reason: value });
+  });
+
+  console.log(validationReportList);
+
+  if (!data?.hasStatusStatuses[0].validationStatus) {
+    return (
+      <div className="tooltip-styles">
+        <p>
+          <span>Validation report for {`${data?.primaryId}`}</span>
+        </p>
+        <div className="ag-theme-alpine" style={{ height: 300, width: 550 }}>
+          <AgGridReact
+            rowData={validationReportList}
+            columnDefs={columnDefs}
+          ></AgGridReact>
+        </div>
+      </div>
+    );
+  } else {
+    return "";
   }
-
-  getGui() {
-    return this.eGui;
-  }
-}
+};
