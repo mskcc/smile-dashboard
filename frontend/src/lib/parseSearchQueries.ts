76859234cd73034d79e462e90aa2c627db3a34ee
Quan Nguyen <86090707+qu8n@users.noreply.github.com>
2023-07-25 14:52:02 -0400

    Add back missing leading 0s

--- frontend/src/lib/parseSearchQueries.ts
+++ frontend/src/lib/parseSearchQueries.ts
@@ -1,8 +1,17 @@
 export function parseSearchQueries(value: string): string[] {
   const queries = value
     .split(/[\s,]+/) // split on whitespaces and commas
-    .map((query) => query.trim())
-    .filter(Boolean); // remove empty strings (e.g. when `value` is `123,,456`)
+    .filter(Boolean) // remove empty strings (e.g. when `value` is `123,,456`)
+    .map((query) => {
+      query = query.trim();
+      // Add back leading 0s to queries that were copied from Excel, where leading 0s
+      // of number cells are removed by default (e.g. `01234` becomes `1234`).
+      const isNumber = /^\d*$/.test(query);
+      if (isNumber) {
+        return query.padStart(5, "0");
+      }
+      return query;
+    });
   const uniqueQueries = Array.from(new Set(queries));
   return uniqueQueries;
 }
