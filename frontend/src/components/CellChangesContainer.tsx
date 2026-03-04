afa460a8105432d957b96823dfe8577764c88991
Lai <laib1@lski5346.mskcc.root.mskcc.org>
2026-03-04 08:17:24 -0500

    add change log fields for data updates

--- frontend/src/components/CellChangesContainer.tsx
+++ frontend/src/components/CellChangesContainer.tsx
@@ -1,7 +1,8 @@
-import { Button, Modal } from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap";
 import { AgGridReact } from "ag-grid-react";
 import { RecordChange } from "../types/shared";
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
+import { NO_CHANGELOG_WARNING } from "../configs/shared";
 
 const updateModalColumnDefs = [
   { field: "recordId", rowGroup: true, hide: true },
@@ -21,7 +22,7 @@ interface CellChangesContainerProps {
   cellChangesHandlers: {
     handleDiscardChanges: () => void;
     handleConfirmUpdates: () => void;
-    handleSubmitUpdates: () => void;
+    handleSubmitUpdates: (author: string, reasonForChange: string) => void;
     showUpdateModal: boolean;
     setShowUpdateModal: Dispatch<SetStateAction<boolean>>;
   };
@@ -38,11 +39,17 @@ export function CellChangesContainer({
   },
   isSampleLevelChanges,
 }: CellChangesContainerProps) {
+  const [changelog, setChangelog] = useState("");
+  const [authorChangelog, setAuthorChangelog] = useState("");
+
   if (isSampleLevelChanges) {
     autoGroupColumnDef.headerName = "Primary ID";
   } else {
     autoGroupColumnDef.headerName = "Cohort ID";
   }
+
+  const disableSubmitUpdates =
+    changelog.trim() === "" || authorChangelog.trim() === "";
 
   function handleUpdateModalHide() {
     setShowUpdateModal(false);
@@ -90,6 +97,26 @@ export function CellChangesContainer({
                 groupDefaultExpanded={1}
               />
             </div>
+            <Form.Group className="d-flex align-items-center mt-3">
+              <Form.Label className="mb-0 me-2 text-nowrap">
+                Reason for Change:
+              </Form.Label>
+              <Form.Control
+                type="text"
+                size="sm"
+                className="me-3"
+                value={changelog}
+                onChange={(e) => setChangelog(e.target.value)}
+              />
+              <Form.Label className="mb-0 me-2 text-nowrap">Author:</Form.Label>
+              <Form.Control
+                style={{ width: "30%" }}
+                type="text"
+                size="sm"
+                value={authorChangelog}
+                onChange={(e) => setAuthorChangelog(e.target.value)}
+              />
+            </Form.Group>
           </Modal.Body>
 
           <Modal.Footer>
@@ -99,9 +126,16 @@ export function CellChangesContainer({
             >
               Cancel
             </Button>
-            <Button className="btn btn-success" onClick={handleSubmitUpdates}>
+            <Button
+              className="btn btn-success"
+              onClick={() => handleSubmitUpdates(authorChangelog, changelog)}
+              disabled={disableSubmitUpdates}
+            >
               Submit Updates
             </Button>
+            {disableSubmitUpdates && (
+              <span className="changelog-alert">{NO_CHANGELOG_WARNING}</span>
+            )}
           </Modal.Footer>
         </Modal>
       )}
